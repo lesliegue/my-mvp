30938e402d700cc3c5533a00403f7e1dbeb61089
Leslie-Guerrero <lesliegue99@gmail.com>
2024-07-30 22:45:19 +0200

    a longer get statement
    harder to read but gets better results, also tried delete

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,37 @@ var express = require('express');
 var router = express.Router();
 const db = require("../model/helper");
 
+async function getAllRecipes(req) {
+  /* ANOTHER WAY TO GET, BUT NOT AS PRETTY
+  brings ingredients info separated by comma, maybe handle it on front
+
+  db("SELECT r.title, r.image, r.description, r.servings, r.notes, GROUP_CONCAT(i.name),GROUP_CONCAT(i.amount) FROM recipe r LEFT JOIN ingredients i ON r.id = i.recipe_id;")
+  */
+
+  //Harder to read, but gets results in a generated json that creates an array for ingredients 
+  let query= `
+    SELECT 
+    JSON_ARRAYAGG(JSON_OBJECT( 'title', r.title, 'image', r.image, 'description', r.description, 'servings', r.servings, 'notes', r.notes, 'ingredient', i.ingredient)) 
+    FROM recipe r LEFT JOIN ( SELECT recipe_id, JSON_ARRAYAGG(JSON_OBJECT('name', name, 'amount', amount, 'measurement unit', measurement_unit)) ingredient 
+    FROM ingredients GROUP BY recipe_id) i ON i.recipe_id = r.id 
+    `
+  const results = await db(query)
+  // results.data returns an array of objects, in this case JSONARRAYGG returns only one full object, so we access that object with [0]
+  //object.values to not return the entire object (key+value) but only the value, but it returns it as an array and we extract that in a variable to have only a string
+    const obj = Object.values(results.data[0]);
+  //creating a json from a string, accessing to position [0] since it comes as an array
+    const response = JSON.parse(obj[0])
+    return response;
+}
+
 /* gets all recipes  */
-router.get('/', function(req, res, next) {
-  db("SELECT r.title, r.image, r.description, r.servings, r.notes, GROUP_CONCAT(i.name),GROUP_CONCAT(i.amount) FROM recipe r LEFT JOIN ingredients i ON r.id = i.recipe_id where r.id = 3;")
-  .then(results => {
-    res.send(results.data);
-  })
-  .catch(err => res.status(500).send(err));
+router.get('/', async function(req, res, next) {
+  try {
+    const recipes = await getAllRecipes(req);
+    res.send(recipes);
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 // // get ONE recipe
@@ -63,16 +87,15 @@ router.post("/ingredients", async (req, res, next) => {
 });
 
 
-router.delete("/:id", async (req, res, next) => {
-  let id = req.params.id;
-  let result = await db(`SELECT * FROM recipe WHERE id = ${id}`);
+router.delete("/:id", async function(req, res, next) {
+  const { id } = req.params;
   try {
-    await db(`DELETE FROM recipe WHERE id = ${id}`);
-    const results = await db(`SELECT * FROM recipe`);
-    // and sent it back to a response
-    res.send(results.data);
+    await db(`DELETE FROM recipe WHERE id=${id}`);
+    await db(`DELETE FROM ingredients WHERE recipe_id = ${id}`);
+    const recipes = await getAllRecipes();
+    res.send(recipes);
   } catch (err) {
-    res.status(500).send({ error: err.message });
+    res.status(500).send(err);
   }
 });
 
